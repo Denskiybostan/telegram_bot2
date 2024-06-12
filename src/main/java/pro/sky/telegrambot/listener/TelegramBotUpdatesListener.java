ae8b15db86ade1fced2141151c669797c182310d
denis <deniz246@mail.ru>
2024-06-12 09:30:39 +0300

    1

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -88,6 +88,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         NotificationTask task = new NotificationTask();
         task.setChatId(chat);
         task.setTaskText(reminderText);
+        task.setTaskClock(reminderClock);
 
     }
     private void sendMessage(Long chat, String text) {
